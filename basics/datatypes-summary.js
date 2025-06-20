862f7e64d4f661b55b9b1e9f0e33766ef72771b2
Pratiksha Katkade <pratikshakatkade1108@gmail.com>
2025-06-20 11:25:15 +0000

    data type summary

---
+++ basics/datatypes-summary.js
@@ -0,0 +1,49 @@
+// Primitive datatypes
+// 7 types : String , Number, Boolean, Undefined, Null, Symbol, BigInt
+
+
+// Reference Type( Non primitive )
+
+// Array, Objects, Functions 
+
+// JavaScript is dynamically typed language variables types are checked at run time 
+// its not needed so define its type of data at time of decalartion 
+
+// Primitive Datatypes
+const score = 100;
+const value =100.3
+
+const isLoggedIn = false
+const temp = null
+let email;
+
+const id = Symbol('123')
+const id_2 = Symbol('123')
+
+console.log(id===id_2);
+
+console.log(id==id_2);
+
+const bignumber = 3456789456789765435678765434567897654n
+console.log(typeof(bignumber));
+
+
+// Non primitive (Refernce) - Array, Object, Functions
+
+const heros =["varun","akshay","ritik","saheer"]
+let obj = {
+      name :"Pratiksha",
+      age :22
+}
+
+console.log(heros);
+console.log(typeof(heros));
+console.log(typeof(obj));
+
+const myfuction= function(){
+    console.log("Hello World");
+    
+}
+
+console.log(typeof myfuction);
+console.log(typeof heros);
